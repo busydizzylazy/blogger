64bf6ad9ed05ef6039a495ff06f039cfed5d05c9
Yin Yen Chong <yinyen116@gmail.com>
2019-09-30 07:16:08 +0000

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,20 @@ Rails.application.routes.draw do
 
   
   
+  get 'users/new'
+
   get  'static_pages/home'
   get  'static_pages/help'
   get  'static_pages/about'
   get  'static_pages/contact'
   get  'static_pages/account'
+  
+  get  '/help',    to: 'static_pages#help', as: 'helf'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
+  get  '/account', to: 'static_pages#account'
+  
+  get  '/signup',  to: 'users#new'
   
   root 'static_pages#home'
   
