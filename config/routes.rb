e7cd8e73a4f79ac4bf5137d137c1315bfa976e9d
Yin Yen Chong <yinyen116@gmail.com>
2019-10-02 02:19:33 +0000

    Bcrypt and login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   resources :users
   
   
@@ -18,7 +20,9 @@ Rails.application.routes.draw do
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
   
-  
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   
   root 'static_pages#home'
   
