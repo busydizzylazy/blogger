076636b1012dbfb881aef3fbeea918b109c17782
Yin Yen Chong <yinyen116@gmail.com>
2019-11-06 09:40:31 +0000

    back again

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -9,6 +9,8 @@ if Rails.env.production?
       :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
       :aws_secret_access_key => ENV['S3_SECRET_ACCESS_KEY']
     }
+    
+    ## something
     config.fog_directory     =  ENV['S3_BUCKET']
     config.fog_provider = 'fog/aws'
     config.storage = :fog
